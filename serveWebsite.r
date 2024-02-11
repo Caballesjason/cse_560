5c245d2b6b43628f8b54a20477fac8147652e86f
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-11 11:04:12 -0500

    first commit!

---
+++ serveWebsite.r
@@ -0,0 +1,13 @@
+run_test_site <- function() {
+  require(bookdown)
+
+  serve_book(dir = getwd(), output_dir = "bookOutput", preview = TRUE,
+             in_session = TRUE, quiet = FALSE)
+  noquote('http://127.0.0.1:4321')
+
+}
+
+run_test_site()
+
+
+
