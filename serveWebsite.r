09939b8a5b8d1b0799036e0b57bbce87f4f9691f
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-11 16:59:34 -0500

    new output directory for website

--- serveWebsite.r
+++ serveWebsite.r
@@ -1,7 +1,7 @@
 run_test_site <- function() {
   require(bookdown)
 
-  serve_book(dir = getwd(), output_dir = "bookOutput", preview = TRUE,
+  serve_book(dir = getwd(), output_dir = "docs", preview = TRUE,
              in_session = TRUE, quiet = FALSE)
   noquote('http://127.0.0.1:4321')
 
