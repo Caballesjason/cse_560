c7324c8484072a81c3ed2ab24838dcb49d95ea38
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-11 22:25:39 -0500

    test

--- serveWebsite.r
+++ serveWebsite.r
@@ -4,7 +4,7 @@ run_test_site <- function() {
   serve_book(dir = getwd(), output_dir = "docs", preview = TRUE,
              in_session = TRUE, quiet = FALSE)
   noquote('http://127.0.0.1:4321')
-
+  file.create(paste(getwd(), "/docs", "/nojekll", sep=''))
 }
 
 run_test_site()
