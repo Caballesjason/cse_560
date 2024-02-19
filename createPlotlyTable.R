865b601be6f1ecfda234c58d89b4dac8f200bb6f
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-19 16:22:25 -0500

    - changed this file name

--- createPlotlyTable.R
+++
@@ -1,9 +0,0 @@
-library(plotly)
-
-createTable <- function(dataSet) {
-fig <- plot_ly(type="table",header=list(values=names(dataSet), fill=list(color = "blue"), 
-                                        font=list(color="white")), cells=list(values=unname(dataSet)))
-
-fig <- fig %>% layout(autosize = T)
-return(fig)
-}
